876a2d1d69fb03c38dd0429551384b398d4644fe
Felix _Coder <67955883+felixmumo@users.noreply.github.com>
2025-05-04 05:10:33 +0300

    Add files via upload

---
+++ salary.java
@@ -0,0 +1,11 @@
+public class Main {
+ int salary = 1000;
+ int bonus = 500;
+    int total = salary + bonus;
+    if (total > 1000) {
+        System.out.println("Total salary is greater than 1000");
+    } else {
+        System.out.println("Total salary is less than or equal to 1000");
+    }
+    System.out.println("Total salary: " + total);   
+}
